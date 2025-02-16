bb5b9c15939a3323e97783fd74a2a92cc4ce617a
fabianarequena <fabianarequena@usf.edu>
2025-02-16 11:59:09 -0500

    Task 1 - Customer Invoice Calculation

---
+++ cc_7.js
@@ -0,0 +1,11 @@
+// Task 1: Customer Invoice Calculation
+
+function calculateInvoice(subtotal, taxRate, discount) {
+    let total = (subtotal + (subtotal * taxRate)) - discount;
+    return `Total Invoice: $${total.toFixed(2)}`;
+}
+console.log(calculateInvoice(100, 0.08, 5)); // Expected: "Total Invoice: $103.00"
+console.log(calculateInvoice(500, 0.1, 20)); // Expected: "Total Invoice: $530.00"
+
+// Here we created a function with 3 parameters and developed a formula to calculate an invoice, followed by returning test data
+

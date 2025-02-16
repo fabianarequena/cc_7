0ef0519894c92a4b3dfc743154b1be317250d196
fabianarequena <fabianarequena@usf.edu>
2025-02-16 12:08:28 -0500

    Task 2 - Employee Hourly Wage Calculation

--- cc_7.js
+++ cc_7.js
@@ -3,9 +3,20 @@
 function calculateInvoice(subtotal, taxRate, discount) {
     let total = (subtotal + (subtotal * taxRate)) - discount;
     return `Total Invoice: $${total.toFixed(2)}`;
-}
+};
 console.log(calculateInvoice(100, 0.08, 5)); // Expected: "Total Invoice: $103.00"
 console.log(calculateInvoice(500, 0.1, 20)); // Expected: "Total Invoice: $530.00"
 
 // Here we created a function with 3 parameters and developed a formula to calculate an invoice, followed by returning test data
 
+
+// Task 2: Employee Hourly Wage Calculatio
+
+function calculateHourlyWage(salary, hoursPerWeek) {
+    let hourlyWage = salary / (hoursPerWeek * 52); 
+    return `Hourly Wage: $${hourlyWage.toFixed(2)}`;
+};
+console.log(calculateHourlyWage(52000, 40)); // Expected: "Hourly Wage: $25.00"
+console.log(calculateHourlyWage(75000, 35)); // Expected: "Hourly Wage: $41.21"
+
+// Here we created a function with 2 parameters and developed a formula to calculate hourly wage, followed by returning test data
